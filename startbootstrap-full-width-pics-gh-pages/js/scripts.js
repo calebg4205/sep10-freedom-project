90d0ad99d8661c7bac3a7a9b1015e12069afa5a1
caleb Garica <calebg23@nycstudents.net>
2024-04-30 20:13:41 -0500

    finished with MVP

--- startbootstrap-full-width-pics-gh-pages/js/scripts.js
+++ startbootstrap-full-width-pics-gh-pages/js/scripts.js
@@ -5,3 +5,33 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+
+
+$(document).ready(function(){
+    $(".button1").click(function(){
+      $("#alex").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".button2").click(function(){
+      $("#alex").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $(".button").click(function(){
+      $("").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".button4").click(function(){
+      $("h1").fadeIn();
+    });
+  });
+
+  var img %('<img id="dynamic">');
+  img.attr('src', IMB_704.jpg);
+  img.appendTO('.IMB');
